d2b4a68f0f3349f0b18b7e20b56554af2e2160a6
sababg <beigisaba@gmail.com>
2026-01-12 22:51:48 -0500

    Add new task to the TaskList with form

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let tasksList = [];
+
 let addNewTaskButton = document.getElementById("addNewTaskButton");
 let taskFormCancelButton = document.getElementById("taskFormCancelButton");
 let taskFormContainer = document.getElementById("taskFormContainer");
+let taskForm = document.getElementById("taskForm");
 let addNewTaskButtonContainer = document.getElementById(
   "addNewTaskButtonContainer"
 );
@@ -13,6 +16,17 @@ addNewTaskButton.addEventListener("click", () => {
 taskFormCancelButton.addEventListener("click", () => {
   changeFormDisplayClass();
   changeAddNewTaskDisplayClass();
+});
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = {
+    name: document.getElementById("taskName").value,
+    category: document.getElementById("category").value,
+    deadline: document.getElementById("deadline").value,
+    status: document.getElementById("status").value,
+  };
+  tasksList.push(task);
 });
 
 const changeFormDisplayClass = () => {
